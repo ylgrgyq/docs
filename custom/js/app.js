40a8b2989c5ba1f6b4a2a7938e4533a0dbb13b53
sunchanglong <sunchang_long@163.com>
2015-03-04 14:17:27 +0800

    element compile,separate app.'s

---
+++ custom/js/app.js
@@ -0,0 +1,70 @@
+//apps data
+angular.module("app", []);
+angular.module("app").controller("AppCtrl", ['$scope', '$http', '$timeout','$compile',
+    function($scope, $http, $timeout, $compile) {
+
+        // $timeout(function(){
+
+        //     // $compile(angular.element('body'),$scope);
+        //     $scope.$apply(refactDom)
+        // },3000);
+        pretty();
+        refactDom($timeout,$scope,$compile);
+        $scope.appid = "{{your_app_id}}";
+        $scope.appkey = "{{your_app_key}}";
+        $http.get("/1/clients/self/apps").success(
+            function(data) {
+                if (data.length > 0) {
+
+                    $scope.currentApp = data[0];
+                    $scope.$watch('currentApp', function() {
+                        $scope.appid = $scope.currentApp.app_id;
+                        $scope.appkey = $scope.currentApp.app_key;
+                    });
+                    $scope.apps = data;
+
+                }
+
+
+            }).error(function(data) {
+
+        });
+
+
+        function refactDom($timeout,$scope,$compile){
+            $("pre.prettyprint code").each(function(index, ele) {
+              $(ele).after("<div class='doc-example-action'><button class='copybtn'>Copy</button></div>");
+              var appsStr = " <div class='doc-example-selector' ng-show='apps.length' ><span>选择应用 <select ng-model='currentApp' ng-options='app.app_name for app in apps'></select></span>";
+              $(ele).after($compile(appsStr)($scope));
+              glueCopy();
+            });
+            // code pretty
+        }
+
+        function pretty(){
+            var pres = document.getElementsByTagName("pre");
+            for (var i = 0; i < pres.length; i++) {
+                pres[i].className = "prettyprint";
+
+            }
+
+            prettyPrint();
+        }
+        function glueCopy(){
+            $(function() {
+              var clip = new ZeroClipboard();
+              clip.glue($(".copybtn"));
+              clip.on("mousedown", function(client, args) {
+                $(this).parents("pre.prettyprint").removeClass("active")
+                clip.setText($(this).parents("pre").find("code").text());
+              });
+              clip.on("complete", function() {
+                $(this).parents("pre.prettyprint").addClass("active");
+              });
+              clip.on('noflash', function() {
+                $(".copybtn").hide();
+              });
+            });
+        }
+    }
+]);
--- custom/js/common.js
+++ custom/js/common.js
@@ -1,3 +1,5 @@
+
+
 // size format
 (function() {
     var prettyBytes = function(num) {
@@ -31,33 +33,7 @@
     }
 })();
 
-// code pretty
-$(function() {
-  var pres = document.getElementsByTagName("pre");
-  for (var i = 0; i < pres.length; i++) {
-      pres[i].className = "prettyprint";
 
-  }
-
-  prettyPrint();
-
-  $("pre.prettyprint code").each(function(index, ele) {
-    $(this).after("<div class='doc-example-action'><button class='copybtn'>Copy</button></div>");
-
-  });
-  var clip = new ZeroClipboard();
-  clip.glue($(".copybtn"));
-  clip.on("mousedown", function(client, args) {
-    $(this).parents("pre.prettyprint").removeClass("active")
-    clip.setText($(this).parents("pre").find("code").text());
-  });
-  clip.on("complete", function() {
-    $(this).parents("pre.prettyprint").addClass("active");
-  });
-  clip.on('noflash', function() {
-    $(".copybtn").hide();
-  });
-});
 
 // doc search
 $(function() {
@@ -68,40 +44,7 @@ $(function() {
     });
 });
 
-//apps data
-angular.module("app", []);
-angular.module("app").controller("AppCtrl", ['$scope', '$http', '$timeout',
-    function($scope, $http, $timeout) {
 
-        $scope.appid = "{{your_app_id}}";
-        $scope.appkey = "{{your_app_key}}";
-        $http.get("/1/clients/self/apps").success(
-            function(data) {
-                if (data.length > 0) {
-
-                    $scope.currentApp = data[0];
-                    $scope.$watch('currentApp', function() {
-                        $scope.appid = $scope.currentApp.app_id;
-                        $scope.appkey = $scope.currentApp.app_key;
-                    });
-                    $scope.apps = data;
-                    $("pre.prettyprint code").each(function(index, ele) {
-                      // $(ele).after("<div class='doc-example-action'><button class='copybtn'>Copy</button></div>");
-                      if ($scope.apps && $scope.apps.length>0) {
-                        // After debuging, re-apply ng-show='currentApp' for .doc-example-selector
-                        // $timeout(function(){
-                        //   $(ele).after("<div class='doc-example-selector' ng-show='currentApp'><span>选择应用 <select ng-model='currentApp' ng-options='app.app_name for app in apps'></select></span>");
-                        // });
-
-                      }
-                    });
-                }
-
-            }).error(function(data) {
-
-        });
-    }
-]);
 
 // sdk version
 $(function(){
