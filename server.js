02252b424b952e756bd3b6615724968e2248c0a8
wchen <sdjcw13@gmail.com>
2015-06-28 16:00:09 +0800

    增加「一套模板多分渲染」的小工具

---
+++ server.js
@@ -0,0 +1,7 @@
+require('coffee-script/register')
+var app = require('./app');
+
+var PORT = parseInt(process.env.PORT || 3001);
+var server = app.listen(PORT, function () {
+  console.log('Node app is running, port:', PORT);
+});
