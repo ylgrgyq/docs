e98ccad1d411c83e3fbb5b03d357da1976f4912d
csun <csun@csundeMacBook-Pro.local>
2015-06-29 11:13:47 +0800

    当所有都加载完运行完再进行 angular boot

--- custom/js/app.js
+++ custom/js/app.js
@@ -256,21 +256,4 @@ angular.module('app').directive('lcComment',['$compile',function($compile){
 
 
 
-$(function(){
-    // $('#content [version]').each(function(k,v){
-    //     var version = $(v).attr('version');
-    //     $(v).append('<div class="toggle-comment" ng-click="showCommentDialog(\''+version+'\''+',$event)">+ <span> {{}}</span> </div>');
-    // })
-
-    $('#content [version]').each(function(k,v){
-        var version = $(v).attr('version');
-        $(v).append('<div version="'+version+'" all-comment="allComment" showDialogMethod="showCommentDialog()" lc-comment> </div>');
-    });
-
-    angular.element(document).ready(function() {
-
-      angular.bootstrap(document, ['app']);
-
-    });
-});
 
