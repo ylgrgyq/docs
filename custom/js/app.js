47065f1e989804821807af14b92f5c2d2f50f39c
csun <csun@csundeMacBook-Pro.local>
2015-07-01 17:53:15 +0800

    has-comments class

--- custom/js/app.js
+++ custom/js/app.js
@@ -237,7 +237,7 @@ angular.module('app').directive('lcComment',['$compile',function($compile){
             version:'@version',
             allComment: '=allComment'
         },
-        template:'<div class="toggle-comment" ng-click="f($event)"><span class="icon icon-chat-alt"></span> <span>{{allComment[version]}}</span></div>',
+        template:'<div class="toggle-comment" ng-class="{\'has-comments\':allComment[version]}" ng-click="f($event)"><span class="icon icon-chat-alt"></span> <span>{{allComment[version]}}</span></div>',
         link: function(scope, element, attrs) {
             scope.f = function(e){
                 scope.$parent.showCommentDialog(e,scope.version);
