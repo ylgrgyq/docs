f8363a822a64829b5c93893dd2ac6275c2ec1e06
尼奥 <1988wangxiao@gmail.com>
2016-06-21 16:19:23 +0800

    「JS 文档」修正文档 GitHub 链接。 (#1262)

--- custom/js/md.js
+++ custom/js/md.js
@@ -104,11 +104,9 @@ var initGitHubLinks = function() {
   var currentPath = window.location.pathname.match(/.*\/(.+).html/i)[1];
   $('#content').prepend("<div class=docs-meta>\
       <span class='icon icon-github'></span>\
-      <a href='http://github.com/leancloud/docs/blob/master/md/" + currentPath + ".md'>在 GitHub 查看</a>\
+      <a href='https://github.com/leancloud/docs'>在 GitHub 查看</a>\
       |\
-      <a href='http://github.com/leancloud/docs/commits/master/md/" + currentPath + ".md'>文件历史</a>\
-      |\
-      <a href='http://github.com/leancloud/docs/edit/master/md/" + currentPath + ".md'>编辑</a>\
+      <a href='https://github.com/leancloud/docs#leancloud-documentation'>编辑</a>\
     </div>");
   $('.sidebar-wrapper #toc').append("<li class=sidebar-meta><a href='#' class=do-expand-all>展开所有</a> <a href='#top' class=back-to-top>返回顶部</a></li>");
 };
