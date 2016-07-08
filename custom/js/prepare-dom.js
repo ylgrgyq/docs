0da1b5705ce9de5b007d36c46e7a908290bb106b
Tunghsiao Liu <t@sparanoid.com>
2016-07-08 12:42:37 +0800

    feat: detect GitHub URLs

--- custom/js/prepare-dom.js
+++ custom/js/prepare-dom.js
@@ -6,10 +6,15 @@
 // $(body).html($com)
 function refactDom(){
     $("pre.prettyprint code").each(function(index, ele) {
+      var githubRegex = /(?:https?:\/\/github\.com)/i;
       var url = getPlayUrl(ele);
       var actions = $("<div class='doc-example-action'><button class='copybtn' title='复制'><span class='icon icon-clipboard'></span></button></div>");
       if (url) {
-        actions.prepend("<a class='runbtn' href='" + url + "' target='_blank' title='在 GitHub 中查看'><span class='icon icon-console-run'></span></a>")
+        if (url.match(githubRegex)) {
+          actions.prepend("<a class='runbtn' href='" + url + "' target='_blank' title='在 GitHub 中查看'><span class='icon icon-github'></span></a>");
+        } else {
+          actions.prepend("<a class='runbtn' href='" + url + "' target='_blank' title='运行'><span class='icon icon-console-run'></span></a>");
+        }
       }
       $(ele).after(actions);
       var appsStr = " <div class='doc-example-selector' ng-show='apps.length' ><span>选择应用 <select ng-model='pageState.currentApp' ng-options='app.app_name for app in apps'></select></span>";
@@ -34,7 +39,7 @@ function glueCopy(){
       var clip = new ZeroClipboard();
       clip.glue($(".copybtn"));
       clip.on("mousedown", function(client, args) {
-        $(this).parents("pre.prettyprint").removeClass("active")
+        $(this).parents("pre.prettyprint").removeClass("active");
         clip.setText($(this).parents("pre").find("code").text());
       });
       clip.on("complete", function() {
